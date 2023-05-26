078aa3a2c1b432ddc760bcbb37cee6faa25a90c6
Serg <Serg.srav@mail.ru>
2023-05-26 18:01:18 +0500

    задача1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Console.WriteLine("Введите число  ");
+//int num=Convert.ToInt32(Console.ReadLine()); 
+
+//Console.WriteLine("Введите число  ");
+//int num2=Convert.ToInt32(Console.ReadLine());
+//newRandom().Next(num,num2);
+//{
+ //   Console.WriteLine();
+//}
+
+//Написатьт программу котрая выводит случайноое число из отрезка(10,99)
+// и показывает наибольшую цыфру числа
+
+
+//int num = new Random().Next(10,100); //выбираем случайное число от 10 до 99, присваеваем его переменной num
+//Console.WriteLine(num);//выводим это число
+ 
+// int num1=num/10; //определяем сколько десятков
+ //int num2=num%10; //определяем количество единиц
+
+ //if(num1>num2)// ставим условие
+//{
+ //   Console.WriteLine(num1);//если num1 больше, то выводим его
+//}
+
+//else
+//{
+ //   Console.WriteLine(num2);// если нет выводим num2 
+//}
